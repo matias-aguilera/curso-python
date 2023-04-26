d6a6e4fed1f3c377538f286b6759e57e05939e99
matias-aguilera <matias.aguilera@alumnos.upla.cl>
2023-04-26 15:41:32 -0400

    clase 26/04/2023 periodo1

---
+++ 2-operadores aritmeticos.py
@@ -0,0 +1,24 @@
+print ("suma")
+print(1 + 1)
+
+print("resta")
+print(5 - 2)
+
+print("multiplicacion")
+print(2 * 2)
+
+print("division")
+print(4/2)
+
+print("modulo")
+print(5 % 2)
+
+print("exponente")
+print(2**2)
+
+print("orden de precedencia para operaciones aritmeticas")
+print("parentesis, potencias, raices, multiplicacion, division, suma, resta")
+print(5+5*2)
+
+print ("investigar otras operaciones aritmeticas y divisiones, raiz cuadrada")
+print("que es una division entera")
---
+++ 3-variables.py
@@ -0,0 +1,15 @@
+numero = 3
+print(numero)
+numero1 = 5
+numero2 = 3
+print(numero1+numero2)
+
+resultado = numero1 + numero2
+print(resultado)
+
+# reglas del nombre de las variables
+#1- las variables no pueden empesar por un numero
+#2- recomendacion: escribir siempre en minuscula
+#3- separar las palabras mediante un guion bajo
+#4- no se pueden escribir espacio
+#5- no pueden contener caracteres especiales
